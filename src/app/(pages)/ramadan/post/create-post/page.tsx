0b6844335596aec99dda1111e53455224cf4f00d
Baiel Sadatbek uulu <bsadatbekuulu@na.edu>
2025-02-28 22:01:53 -0600

    t

--- src/app/(pages)/ramadan/post/create-post/page.tsx
+++ src/app/(pages)/ramadan/post/create-post/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import createPostAction from "@/actions/postActions";
 import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 import {
   Form,
   FormControl,
@@ -21,12 +22,10 @@ import { cn } from "@/lib/utils";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { format } from "date-fns";
 import { CalendarIcon } from "lucide-react";
-import { Calendar } from "@/components/ui/calendar";
 import { useRouter } from "next/navigation";
 import { startTransition } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { useFetchPost } from "@/hooks/use-fetch-single-post";
 // import { format } from "path";
 
 const formSchema = z.object({
@@ -39,7 +38,7 @@ const formSchema = z.object({
     message: "Gender is required",
   }),
   hostDate: z.date().refine((val) => val !== undefined, {
-    message: "Host date is required",
+    message: "Host date is required.",
   }),
 });
 
